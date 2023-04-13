f035dab03563bfffd47d04667f89aa313e2191f3
raymo <raymo@Verstappen.dhcp.wustl.edu>
2023-04-13 13:38:16 -0500

    sadd

--- src/studio9/NameToHeight.java
+++ src/studio9/NameToHeight.java
@@ -18,10 +18,27 @@ public class NameToHeight {
 	 * contain a specified name.
 	 */
 	public static void main(String[] args) {
+		
+		Map<String, Integer> hm = new HashMap <String, Integer>();
+		
+		System.out.println("How many people in group?");
 		Scanner in = new Scanner(System.in);
-
-		// FIXME
-		throw new NotYetImplementedException();
+		int N = in.nextInt();
+		for(int i=0; i<N; i++) {
+			System.out.println("Name?");
+			String name = in.next();
+			System.out.println("Height (In Inches)");
+			int height = in.nextInt();
+			hm.put(name, height);
+		}
+		
+		//for-each loop
+		 for (Map.Entry<String, Integer> me : hm.entrySet()) {
+ 
+            // Printing keys
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+		 }
 
 	}
 }
